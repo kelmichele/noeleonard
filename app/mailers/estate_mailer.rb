90edd6c8b5f1d473a4d8d91910b4b221944b584d
kelmichele <kelli@hsdesignhouse.com>
2020-05-11 15:33:01 -0500

    added new images

--- app/mailers/estate_mailer.rb
+++ app/mailers/estate_mailer.rb
@@ -6,7 +6,7 @@ class EstateMailer < ApplicationMailer
     @estate = estate
 
     mail subject: "Wills, Trusts, Estates Case Evaluation Form From Website"
-    # mail to: "kelli@hsdesignhouse.com"
-    mail to: "noel@attorney-leonard.com, noel.leonard.urgent@gmail.com, cyndi@attorney-leonard.com, cyndi.office.urgent@gmail.com"
+    mail to: "kelli@hsdesignhouse.com"
+    # mail to: "noel@attorney-leonard.com, noel.leonard.urgent@gmail.com, cyndi@attorney-leonard.com, cyndi.office.urgent@gmail.com"
   end
 end
