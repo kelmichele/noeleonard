44fbffa5230af59f9f92907ce39ed287493169f0
kelmichele <kelli@hsdesignhouse.com>
2020-05-11 15:38:01 -0500

    first 2 forms still good with updates. Testing last one now.

--- app/mailers/mobileal_mailer.rb
+++ app/mailers/mobileal_mailer.rb
@@ -6,7 +6,7 @@ class MobilealMailer < ApplicationMailer
     @mobileal = mobileal
 
     mail subject: "Mobile Case Evaluation Form From Website"
-    # mail to: "kelli@hsdesignhouse.com"
-    mail to: "noel@attorney-leonard.com, noel.leonard.urgent@gmail.com, cyndi@attorney-leonard.com, cyndi.office.urgent@gmail.com"
+    mail to: "kelli@hsdesignhouse.com"
+    # mail to: "noel@attorney-leonard.com, noel.leonard.urgent@gmail.com, cyndi@attorney-leonard.com, cyndi.office.urgent@gmail.com"
   end
 end
